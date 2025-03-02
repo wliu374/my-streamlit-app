daf836612d040cd9a93cd67c5379fef1db6e5684
wliu374 <wliu374@wisc.edu>
2025-03-02 03:56:48 -0600

    Add main page sliding window images

--- app.py
+++ app.py
@@ -1,13 +1,156 @@
 import streamlit as st
+import os
+import streamlit.components.v1 as components
+
+
+import base64
+
+# Function to encode an image to Base64
+def get_base64_encoded_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode()
+
+# Encode the local image
+image_path = "images/AIR_Lab_June2024.jpg"
+base64_image = get_base64_encoded_image(image_path)
 
 # ✅ Set the page layout to wide (full width)
 st.set_page_config(layout="wide")
 
 st.title("Welcome to UNL AIR Lab")
-# Back to main page
-# if st.button("Go Back to Home"):
-#     st.switch_page("app.py")
-# if st.button("Inference"):
-#     st.switch_page("cv_inference.py")
 
+components.html(
+    """
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
+img {vertical-align: middle;}
 
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+</style>
+</head>
+<body>
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src=
+"https://bse.unl.edu/sites/unl.edu.ianr.biological-systems-engineering/files/styles/4_3_720x540/public/media/image/AIR_Lab_June2024.jpg?itok=69O-Yqn1"   style="width:100%">
+  <div class="text"></div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src=
+"https://bse.unl.edu/sites/unl.edu.ianr.biological-systems-engineering/files/styles/4_3_720x540/public/media/image/Ag_Intelligence_Lab_1.jpg?itok=cfZWvXFK"
+ style="width:100%">
+  <div class="text"></div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src=
+"https://bse.unl.edu/sites/unl.edu.ianr.biological-systems-engineering/files/styles/4_3_720x540/public/media/image/Intelligent_Spraying_Drone.jpg?itok=6evMX_jT"
+ style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+
+<script>
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3500); // Change image every 4 seconds
+}
+</script>
+
+</body>
+</html> 
+
+    """,
+    height=600,
+)
